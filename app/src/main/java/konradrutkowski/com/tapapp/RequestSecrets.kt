f4f40dba6892dba16610b71688c8b43c9c85f396
Konrad <konradxrutkowski@gmail.com>
2018-01-28 18:06:22 +0100

    removed bad things

---
+++ app/src/main/java/konradrutkowski/com/tapapp/RequestSecrets.kt
@@ -0,0 +1,8 @@
+package konradrutkowski.com.tapapp
+
+class RequestSecrets {
+    companion object {
+        const val CLIENT_ID = "Y0MZQFKCI3IOQGLL415Z0WXPT4AWNFTDT0AATPENPOE2TLPR"
+        const val CLIENT_SECRET = "EEBJBSXSKDHIILY1X2JBPVDB5OUNTDECT1Y0DSRKPHSYBVHD"
+    }
+}
