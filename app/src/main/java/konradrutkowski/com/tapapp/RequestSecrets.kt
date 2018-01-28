f0cf5dc49e3f1740645230f861fa7b0b03ecee72
Konrad Rutkowski <konradxrutkowski@gmail.com>
2018-01-28 18:07:41 +0100

    rm
    mrm

--- app/src/main/java/konradrutkowski/com/tapapp/RequestSecrets.kt
+++
@@ -1,8 +0,0 @@
-package konradrutkowski.com.tapapp
-
-class RequestSecrets {
-    companion object {
-        const val CLIENT_ID = "Y0MZQFKCI3IOQGLL415Z0WXPT4AWNFTDT0AATPENPOE2TLPR"
-        const val CLIENT_SECRET = "EEBJBSXSKDHIILY1X2JBPVDB5OUNTDECT1Y0DSRKPHSYBVHD"
-    }
-}
